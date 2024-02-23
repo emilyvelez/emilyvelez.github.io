b882f675292813d1457a5a9237abd41803f83790
Emily Velez <emilyvelez@Emilys-MacBook-Pro-2.local>
2024-02-22 20:10:07 -0500

    Changed 'about me'

--- src/components/about.js
+++ src/components/about.js
@@ -7,9 +7,7 @@ function About(props) {
         <div className="about">
             <div><img src={prof_pic} alt="profile pic"/></div>
             <div className="about-content">
-                I'm a 
-                Data Analyst
-                from Toronto, Canada
+            Toronto-based data enthusiast exploring new opportunities to leverage my skills and passion for data
             </div>
         </div>
     )
